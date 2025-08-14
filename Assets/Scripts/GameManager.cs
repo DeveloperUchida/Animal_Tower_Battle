31d60347a72ee381f1bf5c6832e020fcb9d50034
DeveloperUchida <kouei2257@gmail.com>
2025-08-14 15:54:10 +0900

    DataUpdate

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,10 +8,30 @@ public class GameManager : MonoBehaviour
     bool isGene = false; //キャラクターが生成されたかどうかのフラグ判定
     GameObject geneChara; //生成されたキャラクター単体;
     bool isInterval = false; //キャラクター生成の間隔を制御するフラグ
+    bool isButtonHover = false; //ボタンがホバーされているかどうかを見る
+    bool isButtonReleasedOnButton = false; //ボタンから指を離した直後
+    public static bool isGameOver = false; //ゲームオーバー状態を管理
+    int score; //スコアを管理
+    [SerializeField] TextMeshProUGUI scoreText; //スコア表示用のTextMeshProUGUI
+    bool isGameStarted = false; //ゲーム開始状態を管理
 
+    void Start()
+    {
+        //ゲーム開始時の初期化
+        isGameOver = false; //ゲーム開始時はゲームオーバーではない
+        isGene = false; //キャラクター生成フラグをリセット
+        isInterval = false; //間隔制御フラグをリセット
+        isButtonHover = false; //ボタンのホバー状態をリセット
+    }
     // Update is called once per frame
     void Update()
     {
+        //ゲームオーバーならこの先処理をさせない
+        if (isGameOver)
+        {
+            return;
+        }
+
         //キャラクターが生成されていないかつキャラが静止している場合
         if (!isGene && !isInterval && !CheckMove())
         {
@@ -18,21 +39,46 @@ public class GameManager : MonoBehaviour
             isGene = true;
         }
         //マウスの左ボタンが離されたとき、かつキャラクターが生成されている場合
-        else if (Input.GetMouseButtonUp(0) && isGene)
+        else if (Input.GetMouseButtonUp(0) && isGene && !isButtonHover)
         {
+
+            if (isButtonReleasedOnButton)
+            {
+                isButtonReleasedOnButton = false; //ボタンから指を離した直後のフラグをリセット
+                return; //ボタンから離した直後は何もしない
+            }
+
             //物理挙動を有効にする
             geneChara.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
             isGene = false; //キャラクター生成フラグをリセット
+            //audioSource.PlayOneShot(fall_se); //落下音を再生
             StartCoroutine(IntervalCoroutine()); //キャラクター生成の間隔を制御
         }
-        //⬇️ここから追加⬇️
-        else if (Input.GetMouseButton(0) && isGene)
+
+        else if (Input.GetMouseButton(0) && isGene && !isButtonHover)
         {
             //マウスの左ボタンが押されている間、キャラクターを移動させる(x座標のみ)
             float mousePositionX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
             geneChara.transform.position = new Vector2(mousePositionX, transform.position.y);
         }
+
     }
+
+    public void RotateCharacter()
+    {
+        if (isGene)
+        {
+            geneChara.transform.Rotate(0, 0, -30);//30度ずつ回転
+            //audioSource.PlayOneShot(rotate_se); //回転音を再生
+            isButtonReleasedOnButton = true; //ボタンから指を離した直後のフラグを立てる
+        }
+
+    }   //マウスカーソルがボタンの上にあるかの状態を更新する
+    public void IsButtonChange(bool isX)
+    {
+        isButtonHover = isX; //ボタンの状態を変更
+    }
+
 
 
     void CreateCaractor()
@@ -67,5 +113,3 @@ public class GameManager : MonoBehaviour
     }
 
 }
-
-
